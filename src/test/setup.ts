506a7b2a819571ce986d2049842d392c18954068
Przemyslaw Cieslik <p.cieslik@zengenti.com>
2026-06-22 11:11:31 +0100

    test: finalise Vitest test runner

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,7 +1,10 @@
 import '@testing-library/jest-dom/vitest';
 
-import { cleanup } from '@testing-library/react';
+import { cleanup, configure } from '@testing-library/react';
 import { afterEach, vi } from 'vitest';
+configure({
+  asyncUtilTimeout: 5_000,
+});
 
 Object.defineProperty(HTMLElement.prototype, 'scrollIntoView', {
   configurable: true,
