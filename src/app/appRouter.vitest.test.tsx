abf9e031eb03e5bd2f32af2db74f943c871ff3c5
Przemyslaw Cieslik <cprzemek91@hotmail.com>
2026-07-24 11:43:39 +0100

    style: apply generated Prettier output

--- src/app/appRouter.vitest.test.tsx
+++ src/app/appRouter.vitest.test.tsx
@@ -1,50 +1,20 @@
 import { describe, it } from 'vitest';
 
-import {
-  runStartupAndRouteBoundaryTests,
-} from './appRouter.tests/startupAndRouteBoundaries';
-import {
-  runDashboardRecentCompanyTests,
-} from './appRouter.tests/dashboardRecentCompanies';
-import {
-  runCompanyListAndCreationTests,
-} from './appRouter.tests/companyListAndCreation';
-import {
-  runCompanyWorkflowStateTests,
-} from './appRouter.tests/companyWorkflowStates';
-import {
-  runCompanyWorkflowEditTests,
-} from './appRouter.tests/companyWorkflowEdit';
-import {
-  runCompanyWorkspaceOverviewRouteTests,
-} from './appRouter.tests/companyWorkspaceOverviewRoutes';
-import {
-  runCompanyWorkspaceChildRouteTests,
-} from './appRouter.tests/companyWorkspaceChildRoutes';
-import {
-  runCompanyWorkspaceMissingNavigationTests,
-} from './appRouter.tests/companyWorkspaceMissingNavigation';
-import {
-  runCompanySwitcherNavigationTests,
-} from './appRouter.tests/companySwitcherNavigation';
-import {
-  runGlobalRoutesAndSettingsTests,
-} from './appRouter.tests/globalRoutesAndSettings';
-import {
-  runAssessmentWorkspaceNavigationTests,
-} from './appRouter.tests/assessmentWorkspaceNavigation';
-import {
-  runAssessmentWorkspaceFilterTests,
-} from './appRouter.tests/assessmentWorkspaceFilters';
-import {
-  runAssessmentWorkspaceOverviewActionTests,
-} from './appRouter.tests/assessmentWorkspaceOverviewActions';
-import {
-  runAssessmentWorkspaceReportsAndMissingTests,
-} from './appRouter.tests/assessmentWorkspaceReportsAndMissing';
-import {
-  runReportDetailsAndFallbackRouteTests,
-} from './appRouter.tests/reportDetailsAndFallbackRoutes';
+import { runStartupAndRouteBoundaryTests } from './appRouter.tests/startupAndRouteBoundaries';
+import { runDashboardRecentCompanyTests } from './appRouter.tests/dashboardRecentCompanies';
+import { runCompanyListAndCreationTests } from './appRouter.tests/companyListAndCreation';
+import { runCompanyWorkflowStateTests } from './appRouter.tests/companyWorkflowStates';
+import { runCompanyWorkflowEditTests } from './appRouter.tests/companyWorkflowEdit';
+import { runCompanyWorkspaceOverviewRouteTests } from './appRouter.tests/companyWorkspaceOverviewRoutes';
+import { runCompanyWorkspaceChildRouteTests } from './appRouter.tests/companyWorkspaceChildRoutes';
+import { runCompanyWorkspaceMissingNavigationTests } from './appRouter.tests/companyWorkspaceMissingNavigation';
+import { runCompanySwitcherNavigationTests } from './appRouter.tests/companySwitcherNavigation';
+import { runGlobalRoutesAndSettingsTests } from './appRouter.tests/globalRoutesAndSettings';
+import { runAssessmentWorkspaceNavigationTests } from './appRouter.tests/assessmentWorkspaceNavigation';
+import { runAssessmentWorkspaceFilterTests } from './appRouter.tests/assessmentWorkspaceFilters';
+import { runAssessmentWorkspaceOverviewActionTests } from './appRouter.tests/assessmentWorkspaceOverviewActions';
+import { runAssessmentWorkspaceReportsAndMissingTests } from './appRouter.tests/assessmentWorkspaceReportsAndMissing';
+import { runReportDetailsAndFallbackRouteTests } from './appRouter.tests/reportDetailsAndFallbackRoutes';
 
 type RouterWorkflowCheck = {
   name: string;
